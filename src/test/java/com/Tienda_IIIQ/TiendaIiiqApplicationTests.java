57599c3ee1151e86c887722534438cd16210353c
YohanMora <53808272+YohanMora@users.noreply.github.com>
2022-09-19 12:26:54 -0600

    Initial commit
    Creación del proyecto inicial

---
+++ src/test/java/com/Tienda_IIIQ/TiendaIiiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIiiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
